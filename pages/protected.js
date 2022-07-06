a8f074465bae328f0d378ce6190b38babd660631
Ze Yen <zeyenchai@gmail.com>
2022-07-07 00:37:52 +0800

    add protected page

--- pages/protected.js
+++ pages/protected.js
@@ -1,8 +1,38 @@
 import React from "react";
 import { getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+// export const getServerSideProps = async (ctx) => {
+//   const session = await getSession(ctx);
+
+//   if (!session)
+//     return {
+//       redirect: {
+//         destination: "/auth/signIn",
+//       },
+//     };
+
+//   return {
+//     props: { session },
+//   };
+// };
 
 const Protected = () => {
-  return <div>Protected</div>;
+  const {push} = useRouter()
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated: () => {
+    push('/auth/signIn')
+    }
+  });
+
+  if (status === "loading") return <div>Loading...</div>;
+
+  if (status === "unauthenticated") {
+    return <div>You are unauthenticated, this is a protected page.</div>;
+  }
+
+  return <div>{session.user.email} You are authenticated</div>;
 };
 
 export default Protected;
